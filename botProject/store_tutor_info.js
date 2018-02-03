8bff61dd9a946ca0748bd7722954e9db7250ca10
rikenshah <rikenmanojshah@gmail.com>
2018-02-03 16:50:33 -0500

    Automated Commit

--- botProject/store_tutor_info.js
+++ botProject/store_tutor_info.js
@@ -1,9 +1,22 @@
-var Botkit = require('botkit'),
-    mongoStorage = require('botkit-storage-mongo')({mongoUri: 'mongodb://<seprojuser:seprojuser123@ds123728.mlab.com:23728/wolftutor', tables: ['user','tutor','subject']}),
-    controller = Botkit.slackbot({
-        storage: mongoStorage
-    });
+var Botkit = require('botkit');
+var mongoStorage = require('botkit-storage-mongo')({mongoUri: 'mongodb://seprojuser:seprojuser123@ds123728.mlab.com:23728/wolftutor', tables: ['user','tutor','subject']});
 
-storage.users.all(function(error, users){
-    console.log(users);
+var controller = Botkit.slackbot({
+			        storage: mongoStorage,
+    			},print_users);
+
+exports.controller = controller
+
+function print_users() {
+	var beans = {id: 'cool', beans: ['pinto', 'garbanzo']};
+	controller.storage.user.save(beans);
+}
+
+controller.storage.user.all(function(err,users) {
+
+  console.log(users)
+
+  if (err) {
+    throw new Error(err);
+  }
 });
