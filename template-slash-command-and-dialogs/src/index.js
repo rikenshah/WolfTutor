23a27c7739e7bb51916671162e1be5e81112fa11
mateenrehan <mateenrehan007@gmail.com>
2018-02-05 19:38:48 -0500

    Become a tutor Dialog

--- template-slash-command-and-dialogs/src/index.js
+++ template-slash-command-and-dialogs/src/index.js
@@ -36,32 +36,71 @@ app.post('/commands', (req, res) => {
       token: process.env.SLACK_ACCESS_TOKEN,
       trigger_id,
       dialog: JSON.stringify({
-        title: 'Submit a helpdesk ticket',
+        title: 'Become a Tutor',
         callback_id: 'submit-ticket',
         submit_label: 'Submit',
         elements: [
           {
-            label: 'Title',
-            type: 'text',
-            name: 'title',
-            value: text,
-            hint: '30 second summary of the problem',
-          },
-          {
-            label: 'Description',
-            type: 'textarea',
-            name: 'description',
-            optional: true,
-          },
-          {
-            label: 'Urgency',
+            label: 'Major',
             type: 'select',
-            name: 'urgency',
+            name: 'major',
             options: [
-              { label: 'Low', value: 'Low' },
-              { label: 'Medium', value: 'Medium' },
-              { label: 'High', value: 'High' },
+              { label: 'Computer Science', value: 'Computer Science' },
+              { label: 'Computer Engineering', value: 'Computer Engineering' },
+              { label: 'Electrical Engineering', value: 'Electrical Engineering' },
+              { label: 'Mechanical Engineering', value: 'Mechanical Engineering' },
+              { label: 'Chemical Engineering', value: 'Chemical Engineering' },
             ],
+          },
+          {
+            label: 'Degree',
+            type: 'select',
+            name: 'degree',
+            options: [
+              { label: 'Bachelors', value: 'Bachelors' },
+              { label: 'Masters', value: 'Masters' },
+              { label: 'Associate', value: 'Associate' },
+              { label: 'High School GED', value: 'High School GED' },
+            ],
+          },
+          {
+            label: 'Subjects',
+            type: 'select',
+            name: 'subject',
+            options: [
+              { label: 'Operating Systems', value: 'Operating Systems' },
+              { label: 'Algorithms', value: 'Algorithms' },
+              { label: 'Software Engineering', value: 'Software Engineering' },
+              { label: 'Processor Design', value: 'Processor Design' },
+            ],
+          },
+          {
+            label: 'Rate',
+            type: 'text',
+            subtype: 'number',
+            name: 'rate',
+            hint: 'If you want to tutor for free then type 0 above',
+          },
+          // {
+          //   label: 'Availability',
+          //   type: 'select',
+          //   name: 'availability',
+          //   options: [
+          //     { label: 'Monday', value: 'Monday' },
+          //     { label: 'Tuesday', value: 'Tuesday' },
+          //     { label: 'Wednesday', value: 'Wednesday' },
+          //     { label: 'Thursday', value: 'Thursday' },
+          //     {label: 'Friday', value: 'Friday'},
+          //     {label: 'Saturday', value: 'Saturday'},
+          //     {label: 'Sunday', value: 'Saturday'},
+          //   ],
+          // },
+
+          {
+            label: 'Summary',
+            type: 'textarea',
+            name: 'summary',
+            optional: true,
           },
         ],
       }),
@@ -71,7 +110,6 @@ app.post('/commands', (req, res) => {
     axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
       .then((result) => {
         debug('dialog.open: %o', result.data);
-        console.log("cdjnscnjdscnkdsjcnjdskc");
         res.send('');
       }).catch((err) => {
         debug('dialog.open call failed: %o', err);
