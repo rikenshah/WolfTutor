d61a149b03172a4d99fc17a3e8e8d2d7bc96e970
mateenrehan <mateenrehan007@gmail.com>
2018-02-15 15:43:03 -0500

    Changed model/user.js to add dynamic values in db

--- botProject/src/model/user.js
+++ botProject/src/model/user.js
@@ -1,12 +1,12 @@
 
 const configure = require('./config');
 
-var user_schema =  new configure.schema({name:'string',email:'string',phone: 'string',points: 'number'},{collection:'user'});
+var user_schema =  new configure.schema({user_id:'string',name:'string',email:'string',phone: 'string',points: 'number'},{collection:'user'});
 var user = configure.mongoose.model('user',user_schema);
 module.exports = {
   create_new_user: function(payload){
     console.log(payload);
-    user.create({name:'xyz',email:'test12@ncsu.edu',phone:'',points:100}, function (err,res) {
+    user.create({user_id:payload.user_id,name:payload.name,email:payload.email,phone:payload.phone,points:100}, function (err,res) {
       if(err) return err;
       console.log('1 entry added');
     });
