9ca13f0e3729b2186c41faa4ebd82bdd08eef80d
rikenshah <rikenmanojshah@gmail.com>
2018-02-18 20:53:51 -0500

    callnack fixed

--- botProject/src/model/subject.js
+++ botProject/src/model/subject.js
@@ -8,7 +8,7 @@ var subject = configure.mongoose.model('subject', subject_schema);
 
 module.exports = {
   // return the list of all subjects
-  get_all_subjects: function() {
+  get_all_subjects: function(callback) {
     subject.find({}, '-_id name', function(err, res) {
       if (err) return err;
       callback(res);
