63795761a9d7346bf6f923f159784151858db707
zpdelong <zacherypd@gmail.com>
2018-04-03 18:04:32 -0400

    Checking in prototype of recommendation algorithm

--- src/module/recommendation_algorithm.js
+++ src/module/recommendation_algorithm.js
@@ -16,8 +16,59 @@ const find = (slackUserId) => {
   return promise;
 };
 
-function Prioritize(people) {
+const SCORE_ATTR = 'weightedScore';
 
+function Prioritize(people) {
+    try{
+        for(let person of people){
+            // For each person, we need to pull out their individual review score, their overall review score, and their previous history to weight.
+            let individualScore = GetIndividualScore(person);
+            let overallScore = GetOverallScore(person);
+            let previousInteractionScore = GetPreviousInteractionScore(person); 
+
+            person[SCORE_ATTR] = CalculateWeightedAverage([individualScore, overallScore, previousInteractionScore], [1, 1, 1]);
+        }
+
+        people = NormalizeAttribute(people, SCORE_ATTR); 
+
+        people = SortPeopleByAttribute(people, SCORE_ATTR);
+
+        return people;
+        
+    }catch (e){
+        console.log("An exception occurred");
+        console.log(e.message);
+    }
+
+}
+
+function GetIndividualScore(person){
+    throw {name : "NotImplementedError", message : "too lazy to implement"}; 
+}
+
+function GetOverallScore(person){
+    throw {name : "NotImplementedError", message : "too lazy to implement"}; 
+}
+
+function GetPreviousInteractionScore(person){
+    throw {name : "NotImplementedError", message : "too lazy to implement"}; 
+}
+
+function CalculateWeightedAverage(scores, weights){
+    let avg = 0;
+    for(var i=0; i<scores.length; i++){
+        avg += scores[i] * weights[i];
+    }
+
+    return (avg / scores.length);
+}
+
+function NormalizeAttribute(objects, attributeToNormalize){
+    throw {name : "NotImplementedError", message : "too lazy to implement"}; 
+}
+
+function SortPeopleByAttribute(objects, attributeToNormalize){
+    throw {name : "NotImplementedError", message : "too lazy to implement"}; 
 }
 
 module.exports = { Prioritize };
