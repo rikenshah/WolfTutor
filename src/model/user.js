0ec303917cd6869eab6be5eeb8d1fb8478f2bde1
mgmetro4 <mgmetro4@gmail.com>
2018-04-07 14:57:35 -0400

    fixed review bug

--- src/model/user.js
+++ src/model/user.js
@@ -25,7 +25,10 @@ module.exports = {
       phone: payload.phone,
       points: 100
     }, function(err, res) {
-      if (err) return err;
+      if(err){
+        console.log(err);
+        return err;
+      }
       return true;
       console.log('1 entry added');
     });
@@ -33,7 +36,10 @@ module.exports = {
   user,
   get_user: function(callback){
     user.findOne({},function(err,user){
-      if(err) return err;
+      if(err){
+        console.log(err);
+        return err;
+      }
       callback(user);
     });
   },
@@ -55,44 +61,54 @@ module.exports = {
       callback(null,user.points);
     });
   }, //End of function
-  // This function will be executed when a user gives review for his session
+
+
+  //REVIEWS - This function will be executed when a user gives review for his session
   give_review: function(payload) {
     console.log(payload);
     reservationModel.get_reservation_by_user_id(payload.user.id, function(err, reservations) {
-      if (err) {
+      if(err){
         console.log(err);
-      } else {
+        return err;
+      }
+      else {
         // Check if there are no reservations for that user
         if (reservations.length == 0) {
           console.log('No reservations');
           action.send_message(payload.channel.id, 'Sorry, you don\'t have any sessions to review', []);
-        } else {
-          var current_date = new Date();
-          var current_hour = current_date.getHours().toString();
-          var current_min = current_date.getMinutes().toString();
-          var current_time = current_hour + current_min; // Curremt Time in 24 hr format(e.g 1420)
-          var today = new Date(current_date.getFullYear(), current_date.getMonth(), current_date.getDate(), 0, 0, 0);
+        } 
+        else {
+          var now = new Date();
+          var today = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0); //get rid of time
+          var current_time = now.getHours().toString() + now.getMinutes().toString() //save "hours"+"minutes"
           var today_reservations = [];
+
           // Iterate through the reservation list and check if there are any reservations today
           // and add them to the today reservation list
           for (var i in reservations) {
-            if (reservations[i].date - today == 0) {
-              if (current_time > reservations[i].to) {
+            // take reservation out of datebase, convert to string, make a date
+            var date = new Date(reservations[i].date.toString())
+            // make a date without time because of werid UTC conversion
+            var fixed_date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+            //same day, after ending reservation time
+            if ((fixed_date - today == 0) && (current_time > reservations[i].to)) {
                 today_reservations.push(reservations[i]);
               }
-            } else {
+            else {
               console.log('Different Dates');
             }
           }
+
           // Check if there are no reservations for that user today
           if (today_reservations.length == 0) {
             action.send_message(payload.channel.id, 'Sorry, you don\'t have any sessions to review for today', []);
-          } else {
+          } 
+          else {
             today_reservations.sort(function(a, b) {
               var num_a = Number(a.to);
               var num_b = Number(b.to);
               return num_b - num_a;
-            });
+              });
             tutor_id = today_reservations[0].tutorid;
             // Enter the reviews in that particular tutor
             TutorModel.tutor.findOneAndUpdate({
@@ -105,7 +121,10 @@ module.exports = {
                 }
               }
             }, function(err, res) {
-              if (err) return err;
+              if(err){
+                console.log(err);
+                return err;
+                }
             });
             // Set the active flag from yes to no
             reservationModel.set_inactive(today_reservations[0]);
