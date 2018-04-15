01c959ca857dfbe1f6d09b28e4124f3f7e32ddc1
zpdelong <zacherypd@gmail.com>
2018-04-15 16:47:07 -0400

    Adding some UI to the 'find a tutor' to allow for increasing weights on certain options

--- src/module/prompt.js
+++ src/module/prompt.js
@@ -13,23 +13,25 @@ const subject_list_prompt = require('./prompt/subject_list_prompt');
 const noreview_schedule_prompt = require('./prompt/noreview_schedule_prompt');
 const slot_booking_prompt = require('./prompt/slot_booking_prompt');
 const booking_confirmation_prompt = require('./prompt/booking_confirmation_prompt');
+const select_desired_attribute_prompt = require('./prompt/select_desired_attribute');
 
 
 module.exports = {
-  become_tutor_prompt : become_tutor_prompt.become_tutor_prompt,
-  tutor_features_prompt : tutor_features_prompt.tutor_features_prompt,
-  add_availability_prompt : add_availability_prompt.add_availability_prompt,
-  add_more_availability_prompt : add_more_availability_prompt.add_more_availability_prompt,
-  add_more_subjects_prompt : add_more_subjects_prompt.add_more_subjects_prompt,
-  create_user_prompt : create_user_prompt.create_user_prompt,
-  add_scheduling_prompt : add_scheduling_prompt.add_scheduling_prompt,
-  add_review_prompt : add_review_prompt.add_review_prompt,
+    become_tutor_prompt : become_tutor_prompt.become_tutor_prompt,
+    tutor_features_prompt : tutor_features_prompt.tutor_features_prompt,
+    add_availability_prompt : add_availability_prompt.add_availability_prompt,
+    add_more_availability_prompt : add_more_availability_prompt.add_more_availability_prompt,
+    add_more_subjects_prompt : add_more_subjects_prompt.add_more_subjects_prompt,
+    create_user_prompt : create_user_prompt.create_user_prompt,
+    add_scheduling_prompt : add_scheduling_prompt.add_scheduling_prompt,
+    add_review_prompt : add_review_prompt.add_review_prompt,
 
-  Tutor_Display_Info : tutor_info_prompt.tutor_info_display,
-  DisplayDate : display_date_prompt.tutor_date_display,
-  TutorReview : review_tutor_prompt.tutor_review_display,
-  SubjectList : subject_list_prompt.subject_list_display,
-  NoreviewSchedule : noreview_schedule_prompt.noreview_schedule,
-  SlotBooking : slot_booking_prompt.slot_booking,
-  BookingConfirmation : booking_confirmation_prompt.booking_confirmation
+    Tutor_Display_Info : tutor_info_prompt.tutor_info_display,
+    DisplayDate : display_date_prompt.tutor_date_display,
+    TutorReview : review_tutor_prompt.tutor_review_display,
+    SubjectList : subject_list_prompt.subject_list_display,
+    NoreviewSchedule : noreview_schedule_prompt.noreview_schedule,
+    SlotBooking : slot_booking_prompt.slot_booking,
+    BookingConfirmation : booking_confirmation_prompt.booking_confirmation,
+    SelectDesiredAttribute: select_desired_attribute_prompt
 };
