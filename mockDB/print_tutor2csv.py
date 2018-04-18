2b21e047db1a2dac9082794fc9c8ffe08b1a76eb
zhazhangqi <zhazhangqi@gmail.com>
2018-04-18 18:06:52 -0400

    adding comments into file

--- mockDB/print_tutor2csv.py
+++ mockDB/print_tutor2csv.py
@@ -6,19 +6,18 @@ import datetime
 
 def main():
     """
-    Access documents in a collection
-    Can be used to calculate average ratings of tutor
+    acess to database collection 'tutor' and 'user', output to a formated csv file to easy compare user interface data. 
     """
 
     #------ Connect to Database ------
-    database = 'admin' #heroku_d754621w == database that is connected to heroku
+    database = 'admin' 
 
-    DIR = os.path.dirname(os.getcwd())
-    load_dotenv(DIR + '/.env')
+    # DIR = os.path.dirname(os.getcwd())
+    # load_dotenv(DIR + '/.env')
 
-    URI = os.getenv('MONICA_MLAB_URI')
-    #client = MongoClient() #defaults to localhost on port 27017
-    client = MongoClient(URI) #or use the mlab URI
+    # URI = os.getenv('MONICA_MLAB_URI')
+    client = MongoClient() #defaults to localhost on port 27017
+    # client = MongoClient(URI) #or use the mlab URI
 
 
     #------ point to a database ------
@@ -31,7 +30,9 @@ def main():
     tutor_list = db.tutor.find()
     user_list = db.user.find()
     user_name = {}
+    #------ print out the header of file ---------
     print 'User ID, User Name,   GPA, Avg Rating,  review rating 1, review rating 2, review rating 3, review rating 4, review rating 5, review rating 6, review rating 7'
+    #------ building a dict from id to name ------
     for user in user_list:
         user_name[user['user_id']] = user['name']
 
@@ -42,9 +43,6 @@ def main():
         print line
         
 
-
-
-   
 
 if __name__ == "__main__":
     main()
